90f4314992d05b1b7ef209a26522663d4d1bc018
jflan91 <justinflanagnit@gmail.com>
2020-09-11 13:36:02 -0400

    fixed up to pep8

--- app/core/admin.py
+++ app/core/admin.py
@@ -21,8 +21,9 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes':  ('wide', ),
-            'fields': ('email','password1', 'password2')
+            'fields': ('email', 'password1', 'password2')
         }),
     )
+
 
 admin.site.register(models.User, UserAdmin)
