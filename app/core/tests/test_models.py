e27e864472f7bb64ef4c8163801486cc0835a297
jflan91 <justinflanagnit@gmail.com>
2020-09-10 22:48:08 -0400

    initial commit

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,39 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test for creating a new user with email"""
+        email = "test@email.com"
+        password = "Testpass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test that email is normailzed when user created"""
+        email = 'test@EMAIL.com'
+        user = get_user_model().objects.create_user(email, 'testpass123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_user_invaild_email(self):
+        """Test error on invalid email"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'testpass123')
+
+    def test_super_user_created(self):
+        """Test creating a super user"""
+        user = get_user_model().objects.create_superuser(
+            "test@email.com",
+            "testpass123"
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
